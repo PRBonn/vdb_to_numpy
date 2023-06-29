4a422b955dbe6e2850d40e8ddcc7fa23d46e69c3
Kutay Yilmaz <kutay.yilmaz@tum.de>
2023-06-29 15:20:37 +0300

    remove utils

--- utils/download_test_data.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-import tests.test_data
-
-print("Downloading all the test_data...")
-[
-    cls()  # call default constructor fo TestData objects
-    for name, cls in tests.test_data.__dict__.items()
-    if isinstance(cls, type) and name not in "abstractclassmethod"
-]
--- utils/install_jupyter_hook.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-echo "[WARNING] Installing jupyter_strip_output.py as pre-commit hook"
-cp ./utils/jupyter_strip_output.py .git/hooks/pre-commit
